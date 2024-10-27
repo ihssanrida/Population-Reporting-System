0094f668a79f4ae7c8dc1a4c043bc1543b5d1ea2
Ihssanrida <ridaridi@roehampton.ac.uk>
2024-10-27 14:29:04 +0300

    create city and country controllers and models

--- src/index.js
+++ src/index.js
@@ -2,13 +2,24 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/db'); // Importing database connection
 
+
+const countryRoutes = require('./routes/countryRoutes');
+const cityRoutes = require('./routes/cityRoutes');
+
 const app = express();
 
+//Set pug as the view engine
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
+//serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use('/', countryRoutes);
+app.use('/', cityRoutes);
+
+//Home route
 app.get('/', (req, res) => {
   res.render('index', { title: 'Population Reporting System' });
 });
