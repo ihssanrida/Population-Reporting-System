6334b1a6d5f9ee287cd6f17a4a0b0651941608d3
Ihssanrida <ridaridi@roehampton.ac.uk>
2024-10-28 03:41:59 +0300

    add filter by country continent region

--- src/models/countryModel.js
+++ src/models/countryModel.js
@@ -2,37 +2,62 @@ const db = require('../config/db');
 
 // Get all countries by population
 const getCountriesByPopulation = (callback) => {
-  const query = 'SELECT Code, Name, Continent, Region, Population, Capital FROM country ORDER BY Population DESC';
+  const query = `
+    SELECT Code, Name, Continent, Region, Population, Capital 
+    FROM country 
+    ORDER BY Population DESC`;
   db.query(query, callback);
 };
 
 // Get countries by continent
 const getCountriesByContinent = (continent, callback) => {
-  const query = 'SELECT Code, Name, Continent, Region, Population, Capital FROM country WHERE Continent = ? ORDER BY Population DESC';
+  const query = `
+    SELECT Code, Name, Continent, Region, Population, Capital 
+    FROM country 
+    WHERE Continent = ? 
+    ORDER BY Population DESC`;
   db.query(query, [continent], callback);
 };
 
 // Get countries by region
 const getCountriesByRegion = (region, callback) => {
-  const query = 'SELECT Code, Name, Continent, Region, Population, Capital FROM country WHERE Region = ? ORDER BY Population DESC';
+  const query = `
+    SELECT Code, Name, Continent, Region, Population, Capital 
+    FROM country 
+    WHERE Region = ? 
+    ORDER BY Population DESC`;
   db.query(query, [region], callback);
 };
 
 // Get top N populated countries globally
 const getTopNPopulatedCountries = (n, callback) => {
-  const query = 'SELECT Code, Name, Continent, Region, Population, Capital FROM country ORDER BY Population DESC LIMIT ?';
+  const query = `
+    SELECT Code, Name, Continent, Region, Population, Capital 
+    FROM country 
+    ORDER BY Population DESC 
+    LIMIT ?`;
   db.query(query, [n], callback);
 };
 
 // Get top N populated countries in a continent
 const getTopNPopulatedCountriesInContinent = (continent, n, callback) => {
-  const query = 'SELECT Code, Name, Continent, Region, Population, Capital FROM country WHERE Continent = ? ORDER BY Population DESC LIMIT ?';
+  const query = `
+    SELECT Code, Name, Continent, Region, Population, Capital 
+    FROM country 
+    WHERE Continent = ? 
+    ORDER BY Population DESC 
+    LIMIT ?`;
   db.query(query, [continent, n], callback);
 };
 
 // Get top N populated countries in a region
 const getTopNPopulatedCountriesInRegion = (region, n, callback) => {
-  const query = 'SELECT Code, Name, Continent, Region, Population, Capital FROM country WHERE Region = ? ORDER BY Population DESC LIMIT ?';
+  const query = `
+    SELECT Code, Name, Continent, Region, Population, Capital 
+    FROM country 
+    WHERE Region = ? 
+    ORDER BY Population DESC 
+    LIMIT ?`;
   db.query(query, [region, n], callback);
 };
 
@@ -42,5 +67,5 @@ module.exports = {
   getCountriesByRegion,
   getTopNPopulatedCountries,
   getTopNPopulatedCountriesInContinent,
-  getTopNPopulatedCountriesInRegion
+  getTopNPopulatedCountriesInRegion,
 };
--- src/routes/countryRoutes.js
+++ src/routes/countryRoutes.js
@@ -1,9 +1,15 @@
+// routes/countryRoutes.js
 const express = require('express');
 const router = express.Router();
 const countryController = require('../controllers/countryController');
 
+// Route for the main countries page that uses the country-specific layout
+router.get('/countries', (req, res) => {
+    res.render('countries/countriesIndex', { title: 'Country Reports' });
+});
+
 // Route for getting all countries by population
-router.get('/countries', countryController.getAllCountriesByPopulation);
+router.get('/countries/all', countryController.getAllCountriesByPopulation);
 
 // Route for getting countries by continent
 router.get('/countries/continent/:continent', countryController.getCountriesByContinent);
@@ -11,13 +17,7 @@ router.get('/countries/continent/:continent', countryController.getCountriesByCo
 // Route for getting countries by region
 router.get('/countries/region/:region', countryController.getCountriesByRegion);
 
-// Route for getting top N populated countries globally
-router.get('/countries/top/:n', countryController.getTopNPopulatedCountries);
-
-// Route for getting top N populated countries in a specific continent
-router.get('/countries/top/continent/:continent/:n', countryController.getTopNPopulatedCountriesInContinent);
-
-// Route for getting top N populated countries in a specific region
-router.get('/countries/top/region/:region/:n', countryController.getTopNPopulatedCountriesInRegion);
+// Unified route for fetching top N countries based on query parameters (scope, continent, region)
+router.get('/countries/top', countryController.getTopNCountries);
 
 module.exports = router;
